f46814ea9a53f99a65998dedd53d87e740c06f75
c00164354@itcarlow.ie <c00164354@itcarlow.ie>
2021-02-13 12:40:51 +0000

    Edited Main_Game

--- client_code/Main_Game/__init__.py
+++ client_code/Main_Game/__init__.py
@@ -5,20 +5,30 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
+clock = 0
+
 class Main_Game(Main_GameTemplate):
-  c = Timer(interval=0.1)
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
+    self.timer_1.interval = 1
     # Any code you write here will run when the form opens.
 
   def submit_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
-    global c
+    global clock
     self.timer_1.interval = 0
-    print(c)
+    print(self.timer_1)
+    print(clock)
     pass
+
+  def timer_1_tick(self, **event_args):
+    """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+    global clock
+    
+    clock += 1
+    pass
+
 
 
 
