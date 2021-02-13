9adefefdea47232fc39359fca6688393eebb2504
c00164354@itcarlow.ie <c00164354@itcarlow.ie>
2021-02-13 13:32:58 +0000

    Edited Main_Game, app config and Landing_Page

---
+++ client_code/Landing_Page/__init__.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import Landing_PageTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Landing_Page(Landing_PageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
+    
--- client_code/Main_Game/__init__.py
+++ client_code/Main_Game/__init__.py
@@ -4,29 +4,29 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import time
 
-clock = 0
 
 class Main_Game(Main_GameTemplate):
+  clock = time.time()
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.timer_1.interval = 0.1
     # Any code you write here will run when the form opens.
 
   def submit_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
-    global clock
     self.timer_1.interval = 0
     print(self.timer_1)
-    print(round(clock, 4))
+    self.clock = time.time() - self.clock
+
+    print(round(self.clock, 3))
     pass
 
   def timer_1_tick(self, **event_args):
     """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-    global clock
     
-    clock += 0.1
+    self.clock += 0.001
     pass
 
 
