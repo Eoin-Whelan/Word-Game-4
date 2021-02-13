ed7c4c1f0a88eb5ef40ff9d765143c349749a9a7
c00164354@itcarlow.ie <c00164354@itcarlow.ie>
2021-02-13 14:02:56 +0000

    Edited server_module, Main_Game, app config and Landing_Page

--- server_code/server_module.py
+++ server_code/server_module.py
@@ -1,9 +1,12 @@
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+from anvil.google.drive import app_files
 
-# This is a server module. It runs on the Anvil server,
+dictionary = app_files.word_game_dictionary
 # rather than in the user's browser.
 #
 # To allow anvil.server.call() to call functions here, we mark
